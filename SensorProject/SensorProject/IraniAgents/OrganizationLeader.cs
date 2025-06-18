662ff6743de6760307c9b74f7a5a76294f66b3b0
mosheshulman <msh36345@gmail.com>
2025-06-18 17:31:52 +0300

    phase24

--- SensorProject/SensorProject/IraniAgents/OrganizationLeader.cs
+++ SensorProject/SensorProject/IraniAgents/OrganizationLeader.cs
@@ -12,38 +12,8 @@ public class OrganizationLeader : IraniAgent
         Counter = 0;
         Attack = true;
         CounterAttack = 3;
-        SensorsToDelete = 2;
+        SensorsToDelete = 1;
         AvilableSensors = 7;
     }
     
-    protected override void Activet()
-    {
-        Counter++;
-        Console.WriteLine(Counter);
-        if (Attack)
-        {
-            if (Counter%CounterAttack==0)
-            {
-                for (int i = 0; i < SensorsToDelete; i++)
-                {
-                    AssignedSensors[AgentManeger.Random(AssignedSensors.Length)]=null;
-                    Console.WriteLine("A sensor has been deleted!!!");
-                }
-                Counter++;
-            }
-
-            if (Counter % 10 == 0)
-            {
-                ExposedSensors = SensorManeger.InitSensors(7, 8);
-                Console.WriteLine("The sensors have been changed!!!");
-            }
-        } 
-        foreach (Sensor sensor in AssignedSensors) 
-        { 
-            if (sensor != null) 
-            { 
-                sensor.SensorActivet(); 
-            } 
-        }
-    }
 }
